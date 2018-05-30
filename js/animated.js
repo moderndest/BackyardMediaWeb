102ef7dea31396dd454d142d857bb67c5c5aa41b
moderndest <rattarasan.c@husky.neu.edu>
2018-05-30 13:26:43 -0400

    kkk

--- js/animated.js
+++ js/animated.js
@@ -1,31 +1,51 @@
-$(document).ready(function() {
-    // Check if element is scrolled into view
-    function isScrolledIntoView(elem) {
-      var docViewTop = $(window).scrollTop();
-      var docViewBottom = docViewTop + $(window).height();
+// $(document).ready(function() {
+//     // Check if element is scrolled into view
+//     function isScrolledIntoView(elem) {
+//       var docViewTop = $(window).scrollTop();
+//       var docViewBottom = docViewTop + $(window).height();
   
-      var elemTop = $(elem).offset().top;
-      var elemBottom = elemTop + $(elem).height();
+//       var elemTop = $(elem).offset().top;
+//       var elemBottom = elemTop + $(elem).height();
   
-      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-    }
-    // If element is scrolled into view, fade it in
-    $(window).scroll(function() {
-      $('.description').each(function() {
-        if (isScrolledIntoView(this) === true) {
-          $(this).addClass('animated fadeInRight');
-        }else {
-            $('.description').removeClass('animated fadeInRight');
-          }
-      });
-    });
-  });
-
-
-// $(window).scroll(function(){
-//     if($(window).scrollTop() <= 1000){
-//       $('.description').addClass('animated fadeInLeft');
-//     } else {
-//       $('.description').removeClass('animated fadeInLeft');
+//       return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 //     }
-// });
+//     // If element is scrolled into view, fade it in
+//     $(window).scroll(function() {
+//       $('.description').each(function() {
+//         if (isScrolledIntoView(this) === true) {
+//           $(this).addClass('animated fadeInRight');
+//         }else {
+//             $('.description').removeClass('animated fadeInRight');
+//           }
+//       });
+//     });
+//   });
+
+
+
+
+$(function(){
+  function onScrollInit( items, trigger ) {
+      items.each( function() {
+      var osElement = $(this),
+          osAnimationClass = osElement.attr('data-os-animation'),
+          osAnimationDelay = osElement.attr('data-os-animation-delay');
+        
+          osElement.css({
+              '-webkit-animation-delay':  osAnimationDelay,
+              '-moz-animation-delay':     osAnimationDelay,
+              'animation-delay':          osAnimationDelay
+          });
+          var osTrigger = ( trigger ) ? trigger : osElement;
+          
+          osTrigger.waypoint(function() {
+              osElement.addClass('animated').addClass(osAnimationClass);
+              },{
+                  triggerOnce: true,
+                  offset: '90%'
+          });
+      });
+  }
+  onScrollInit( $('.os-animation') );
+  onScrollInit( $('.staggered-animation'), $('.staggered-animation-container') );
+});
