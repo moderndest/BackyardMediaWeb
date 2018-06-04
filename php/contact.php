ac2c0947ccded0bb3c9c103a2d45d11dda1800a9
moderndest <rattarasan.c@husky.neu.edu>
2018-06-04 04:47:30 -0400

    send mail still error

--- php/contact.php
+++ php/contact.php
@@ -1,11 +1,24 @@
 <?php
 
+echo (extension_loaded('openssl')?'SSL loaded':'SSL not loaded')."\n"; 
+// $fp = fsockopen('localhost', 587, $errno, $errstr, 10);
+// if (!$fp) {
+//     echo "$errstr ($errno)\n";
+// } else {
+//     $out = "QUIT\r\n";
+//     fwrite($fp, $out);
+//     while (!feof($fp)) {
+//         echo fgets($fp, 128);
+//     }
+//     fclose($fp);
+// }
 /*
 THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
 */
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
@@ -19,19 +32,26 @@ require '../vendor/autoload.php';
 
 
 // an email address that will be in the From field of the email.
-$fromEmail = $_POST['uemail'];
-$fromName = $_POST['uname'];
+// $fromEmail = $_POST['email'];
+// $fromName = $_POST['name'];
+// $message = $_POST['notes'];
+$fromEmail = 'modern445@gmail.com';
+$fromName = 'Hello';
+$message = 'I want to work with u';
 
 // $notes = $_POST['message'];
 
 // an email address that will receive the email with the output of the form
-$sendToEmail = 'test@domain.com';
+$sendToEmail = 'modern445@gmail.com';
 $sendToName = 'BackyardMedia';
 
 // smtp credentials and server
+
+
 $smtpHost = 'smtp.mailtrap.io';
 $smtpUsername = 'a618b4e9afd1d4';
 $smtpPassword = '7ac6d272ace22a';
+
 
 
 // subject of the email
@@ -39,7 +59,7 @@ $subject = 'New message from contact form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('uname' => 'Name', 'uemail' => 'Email', 'message' => 'Message'); 
+$fields = array('name' => $fromName, 'email' => $fromEmail, 'message' => $message); 
 
 // message that will be displayed when everything is OK :)
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
@@ -57,7 +77,7 @@ error_reporting(E_ALL & ~E_NOTICE);
 try
 {
 
-    if(count($_POST) == 0) throw new \Exception('Form is empty');
+    // if(count($_POST) == 0) throw new \Exception('Form is empty');
     
     $emailTextHtml = "<h1>You have a new message from your contact form</h1><hr>";
     $emailTextHtml .= "<table>";
@@ -69,7 +89,7 @@ try
         }
     }
     $emailTextHtml .= "</table><hr>";
-    $emailTextHtml .= "<p>Have a nice day,<br>Best,<br>Ondrej</p>";
+    $emailTextHtml .= "<p>Have a nice day,<br>Best,<br>Backyerd Media</p>";
     
     $mail = new PHPMailer;
 
@@ -87,13 +107,20 @@ try
     
     
     $mail->isSMTP();
+    $mail->SMTPOptions = array(
+        'ssl' => array(
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+            'allow_self_signed' => true
+        )
+    );
 
     //Enable SMTP debugging
     // 0 = off (for production use)
     // 1 = client messages
     // 2 = client and server messages
-    $mail->SMTPDebug = 1;
-    $mail->Debugoutput = 'html';
+    $mail->SMTPDebug = 2;
+    //$mail->Debugoutput = 'html';
 
     //Set the hostname of the mail server
     // use
@@ -102,24 +129,30 @@ try
     $mail->Host = gethostbyname($smtpHost);
     
     //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-    $mail->Port = 25;
+    $mail->Port = 2525;
+    // $mail->Port = 587;
+
     //Set the encryption system to use - ssl (deprecated) or tls
-    $mail->SMTPSecure = 'tls';
+    $mail->SMTPSecure = 'ssl';
     
     //Whether to use SMTP authentication
     $mail->SMTPAuth = true;
+    // $mail->AuthType = 'LOGIN';
     
     //Username to use for SMTP authentication - use full email address for gmail
     $mail->Username = $smtpHost;
     
     //Password to use for SMTP authentication
     $mail->Password = $smtpPassword;
+    $mail->SMTPAutoTLS = false;
+  
     
     if(!$mail->send()) {
         throw new \Exception('Message could not be sent. Mailer Error: ' . $mail->ErrorInfo);
     }
     
     $responseArray = array('type' => 'success', 'message' => $okMessage);
+    //$mail->smtpClose();
 }
 catch (\Exception $e)
 {
