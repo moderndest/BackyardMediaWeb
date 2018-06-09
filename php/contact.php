71fca9cebb27005f6c0864d94deb742aa0dc7889
moderndest <rattarasan.c@husky.neu.edu>
2018-06-08 23:58:47 -0400

    fix send email

--- php/contact.php
+++ php/contact.php
@@ -1,173 +1,131 @@
+
 <?php
-//  Backyard Media 
+// Backyard Media 
 // Filename: contact.php
 
 // Author:Chatsuda Rattarasan 
 // Date: May 29 2018  
 
-echo (extension_loaded('openssl')?'SSL loaded':'SSL not loaded')."\n"; 
-// $fp = fsockopen('localhost', 587, $errno, $errstr, 10);
-// if (!$fp) {
-//     echo "$errstr ($errno)\n";
-// } else {
-//     $out = "QUIT\r\n";
-//     fwrite($fp, $out);
-//     while (!feof($fp)) {
-//         echo fgets($fp, 128);
-//     }
-//     fclose($fp);
-// }
-/*
-THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
-*/
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
-//Load Composer's autoloader
+
+//Import the PHPMailer class into the global namespace
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+//SMTP needs accurate times, and the PHP time zone MUST be set
+//This should be done in your php.ini, but this is how to do it if you don't have access to that
+date_default_timezone_set('Etc/UTC');
 require '../vendor/autoload.php';
 
 
-
-/*
- *  CONFIGURE EVERYTHING HERE
- */
-
-
-// an email address that will be in the From field of the email.
-// $fromEmail = $_POST['email'];
-// $fromName = $_POST['name'];
-// $message = $_POST['notes'];
-if(isset($_POST['submit'])){
     $fromEmail = $_POST['email'];
     $fromName = $_POST['name'];
-    $message = $_POST['notes'];
-}
+    $message = $_POST['message'];
 
-$fromEmail = 'modern445@gmail.com';
-$fromName = 'Hello';
-$message = 'I want to work with u';
-
-// $notes = $_POST['message'];
-
-// an email address that will receive the email with the output of the form
-$sendToEmail = 'modern445@gmail.com';
-$sendToName = 'BackyardMedia';
 
 // smtp credentials and server
-
-
 $smtpHost = 'smtp.mailtrap.io';
-$smtpUsername = "6c7f550429c56b";
-$smtpPassword = "a91307364d02a3";
+$smtpUsername = '6c7f550429c56b';
+$smtpPassword = 'a91307364d02a3';
 
-
-
-// subject of the email
-$subject = 'New message from contact form';
 
 // form field names and their translations.
 // array variable name => Text to appear in the email
-$fields = array('name' => $fromName, 'email' => $fromEmail, 'message' => $message); 
+$fields = array('name' => $fromName, 'email' => $fromEmail); 
 
 // message that will be displayed when everything is OK :)
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 
 // If something goes wrong, we will display this message.
 $errorMessage = 'There was an error while submitting the form. Please try again later';
-
-/*
- *  LET'S DO THE SENDING
- */
-
 // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
 error_reporting(E_ALL & ~E_NOTICE);
 
 try
 {
-
-    // if(count($_POST) == 0) throw new \Exception('Form is empty');
-    
-    $emailTextHtml = "<h1>You have a new message from your contact form</h1><hr>";
-    $emailTextHtml .= "<table>";
-
-    // foreach ($_POST as $key => $value) {
-    //     // If the field exists in the $fields array, include it in the email
-    //     if (isset($fields[$key])) {
-    //         $emailTextHtml .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
-    //     }
-    // }
-    $emailTextHtml .= "</table><hr>";
-    $emailTextHtml .= "<p>Have a nice day,<br>Best,<br>Backyerd Media</p>";
-    
+    //Create a new PHPMailer instance
     $mail = new PHPMailer;
-    
+    //Tell PHPMailer to use SMTP
     $mail->isSMTP();
-    // $mail->SMTPOptions = array(
-    //     'ssl' => array(
-    //         'verify_peer' => false,
-    //         'verify_peer_name' => false,
-    //         'allow_self_signed' => true
-    //     )
-    // );
+
 
     //Enable SMTP debugging
     // 0 = off (for production use)
     // 1 = client messages
     // 2 = client and server messages
-    $mail->SMTPDebug = 4;
-    $mail->Debugoutput = 'html';
+    $mail->SMTPDebug = 2;
 
     //Set the hostname of the mail server
-    // use
-    // $mail->Host = gethostbyname('smtp.gmail.com');
-    // if your network does not support SMTP over IPv6
-    $mail->Host = gethostbyname($smtpHost);
+    $mail->Host = $smtpHost;
 
-
-     //Username to use for SMTP authentication - use full email address for gmail
-     $mail->Username = $smtpHost;
-    
-     //Password to use for SMTP authentication
-     $mail->Password = $smtpPassword;
-
-    
-     //Set the encryption system to use - ssl (deprecated) or tls
-     $mail->SMTPSecure = 'tls';
-    
     //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-    // $mail->Port = 2525;
     $mail->Port = 2525;
-    
+
+    //Set the encryption system to use - ssl (deprecated) or tls
+    $mail->SMTPSecure = 'tls';
+    //Custom connection options
+    //Note that these settings are INSECURE
+    $mail->SMTPOptions = array(
+        'ssl' => [
+            'verify_peer' => true,
+            'verify_depth' => 3,
+            'allow_self_signed' => true,
+            'peer_name' => 'smtp.mailtrap.io',
+            'cafile' => '/opt/lampp/etc/ssl.crt/cacert.pem',
+        ],
+    );
     //Whether to use SMTP authentication
     $mail->SMTPAuth = true;
-    // $mail->SMTPSecure = false;
-    $mail->AuthType = 'LOGIN';
-    
-   
-    $mail->SMTPAutoTLS = false;
-    
-     //Recipients 
-     $mail->setFrom($fromEmail, $fromName);
-     $mail->addAddress($sendToEmail, $sendToName); // you can add more addresses by simply adding another line with $mail->addAddress();
-     $mail->addReplyTo($from);
-     
-      //Content
-     $mail->isHTML(true);
- 
-     $mail->Subject = $subject;
-     $mail->Body    = $emailTextHtml;
-     $mail->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
-     
-    
-    if(!$mail->send()) {
-        throw new \Exception('Message could not be sent. Mailer Error: ' . $mail->ErrorInfo);
+    //Username to use for SMTP authentication - use full email address for gmail
+    $mail->Username = $smtpUsername;
+    //Password to use for SMTP authentication
+    $mail->Password = $smtpPassword;
+
+    //Set who the message is to be sent from
+    $mail->setFrom($fromEmail, $fromName);
+
+    //Set who the message is to be sent to
+    $mail->addAddress('whoto@example.com', 'Backyard Media');
+
+    //Set the subject line
+    $mail->Subject = 'New message from contact form';
+
+
+  
+
+    $emailTextHtml = "<div style='width:640px;'>";
+    $emailTextHtml .= "<br><h2>You have a new message from your contact form</h2><hr>";
+
+    $emailTextHtml .= "<br><table>";
+
+    foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email
+        if (isset($fields[$key])) {
+            $emailTextHtml .= "<tr><th>$key : </th><td> $value</td></tr>";
+        }
     }
-    
-    $responseArray = array('type' => 'success', 'message' => $okMessage);
-    //$mail->smtpClose();
+    $emailTextHtml .= "</table><br><hr>";
+    $emailTextHtml .="<p><strong>message : </strong></p>";
+    $emailTextHtml .=" <p style='width:500px;>$message </p></div><hr>";
+    $emailTextHtml .= "<p><br>Best,<br>Backyerd Media supporting team</p>";
+
+
+
+    //Read an HTML message body from an external file, convert referenced images to embedded,
+    //convert HTML into a basic plain-text alternative body
+    // $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+    $mail->msgHTML($emailTextHtml);
+
+
+
+    //Send the message, check for errors
+    if (!$mail->send()) {
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+        throw new \Exception('Message could not be sent. Mailer Error: ' . $mail->ErrorInfo);
+    } else {
+        echo 'Message sent!';
+        $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
 }
 catch (\Exception $e)
 {
