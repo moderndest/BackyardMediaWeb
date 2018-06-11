8f5cdae0368bb58ba1ba2eb1530b964ceb655fea
moderndest <rattarasan.c@husky.neu.edu>
2018-06-11 03:41:22 -0400

    db

--- php/connectDB.php
+++ php/connectDB.php
@@ -1,7 +1,9 @@
 <?php
     $dbhost = "35.186.178.70"; //Defaults to mysqli.default_host
-	$username = getenv('MYSQL_USER');
-	$password = getenv('MYSQL_PASSWORD');
+    // $username = getenv('MYSQL_USER');
+    $username = 'backyardmedia' ;
+    $password = 'backyardmedia2018' ;
+	// $password = getenv('MYSQL_PASSWORD');
 	$database = "BackyardMedia"; 
 	$socket = "mysql:host=$dbhost;dbname=$database";
     
